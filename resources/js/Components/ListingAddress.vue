3ab31c6dc735b74e8fcb7c9fbfe09c6abafbc565
Shaun Thornburgh <personal@shaunthornburgh.com>
2022-12-28 16:17:39 +0000

    Index and show pages

---
+++ resources/js/Components/ListingAddress.vue
@@ -0,0 +1,9 @@
+<template>
+    <span>{{ listing.street }}, {{ listing.street_nr }}, {{ listing.city }} for £{{ listing.price }}</span>
+</template>
+
+<script setup>
+defineProps({
+    listing: Object
+})
+</script>
